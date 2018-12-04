1066ae7232b2dc8233ea24af4c556db3373ff7b7
tovilu <43234043+tovilu@users.noreply.github.com>
2018-12-04 17:08:31 +0100

    Add files via upload

--- src/main/resources/static/juego/Lobby.js
+++ src/main/resources/static/juego/Lobby.js
@@ -3,13 +3,19 @@ var Gentleball = Gentleball || {};
 var fondo;
 var play;
 var LobbyText;
-
+var exit;
+var servidorapagado=true;
 
 var ipserver;
-var usuario;
 ipserver = prompt('Introduzca la ip del servidor:','192.168.1.1:8080');
-usuario = prompt('Introduzca su nombre de usuario','');
 
+
+var iplocal;
+
+$.getJSON('https://api.ipify.org?format=json', function(data){
+    console.log(data.ip);
+	iplocal=data.ip;
+});
 
 Gentleball.Lobby = function(){};
 
@@ -25,150 +31,113 @@ Gentleball.Lobby.prototype = {
         LobbyText = this.game.add.tileSprite(this.game.world.centerX-125, this.game.world.centerY-250, 250, 150, 'LobbyText');
     	
         play = this.game.add.button(this.game.world.centerX-115, this.game.world.centerY+100,'BotonPlay', this.actionOnClick1, this,1,0);
- 
-        setInterval(loadChats,3000);
+        
+        exit = this.game.add.button(947.36, 13.16, 'exit', this.actionOnClick2, this, 1, 0);
+        
+        setInterval(loadJugadores,3000);
   	},
 
   actionOnClick1: function () 
   {
     this.game.state.start('Game'); 
   },
+  
+  actionOnClick2: function () 
+  {
+    this.game.state.start('Tipo'); 
+  },
 
 };
 
-$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
-	  console.log(JSON.stringify(data, null, 2));
-	});
 
-//Cargar chats del servidor
-function loadChats(callback){
-	$.ajax({
-		url: 'http://'+ipserver+'/chats'
-	}).done(function(chats){
-		console.log('Chats loaded: ' + JSON.stringify(chats));
-		if (callback) 
-		{
-		    callback(chats);
-		}
-	})
-}
 
-//Crear chat en el servidor
-function createChat(chat, callback){
-	$.ajax({
-		method: "POST",
-		url: 'http://'+ipserver+'/chats',
-		data: JSON.stringify(chat),
-		processData: false,
-		headers: {
-			"Content-Type": "application/json"
-		}
-	}).done(function(chat){
-		console.log("Chat created" + JSON.stringify(chat));
-		callback(chat);
-	})
-}
-
-//Actualizar chat en el servidor
-function updateChat(chat) {
+//Load jugadores from server
+function loadJugadores(callback) {
     $.ajax({
-        method: 'PUT',
-        url: 'http://'+ipserver+'/chats/'+chat.id,
-        data: JSON.stringify(chat),
+        url: 'http://'+ipserver+'/jugadores'
+    }).done(function (jugadores){
+        console.log('Jugadores loaded: ' + JSON.stringify(jugadores));
+        if (callback) 
+		{
+		    callback(jugadores);
+		}
+		servidorapagado=false;
+    }).fail(function (){
+		console.error("Se ha perdido la conexion con el servidor");
+		servidorapagado=true;
+	})
+}
+
+//Create jugador in server
+function createJugador(jugador, callback) {
+    $.ajax({
+        method: "POST",
+        url: 'http://'+ipserver+'/jugadores',
+        data: JSON.stringify(jugador),
         processData: false,
         headers: {
             "Content-Type": "application/json"
         }
-    }).done(function (chat) {
-        console.log("Updated chat: " + JSON.stringify(chat))
+    }).done(function (jugador) {
+        console.log("Jugador created: " + JSON.stringify(jugador));
+        callback(jugador);
     })
 }
 
-//Borrar chat del servidor
-function deleteChat(chatId) {
+//Update jugador in server
+function updateJugador(jugador) {
+    $.ajax({
+        method: 'PUT',
+        url: 'http://'+ipserver+'/jugadores/' + jugador.ip,
+        data: JSON.stringify(jugador),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (jugador) {
+        console.log("Updated jugadores: " + JSON.stringify(jugador))
+    })
+}
+
+//Delete jugador from server
+function deleteJugador(jugadorIp) {
     $.ajax({
         method: 'DELETE',
-        url: 'http://'+ipserver+'/chats/'+chatId
-    }).done(function (chat) {
-        console.log("Se borro el chat "+chatId)
+        url: 'http://'+ipserver+'/jugadores/' + jugadorIp
+    }).done(function (jugador) {
+        console.log("Deleted jugadores " + jugadorIp)
     })
 }
 
-//Show Chat in page
-function showChat(chat) {
 
-    var checked = '';
-    var style = '';
-    
-    $('#mensaje').append('<div id="chat-'+chat.id+'"><span '+style+'>'+usuario+": "+chat.value+'</span>')
-}
 
-$(document).ready(function () {
-	var ref=0;
-    loadChats(function (chats) {   	
-        //When Chats are loaded from server
-        for (var i = ref; i < chats.length; i++) 
-        {
-            showChat(chats[i]);
-        }
-        ref=chats.length;
+$(document).ready(function () 
+{
+	
+    loadJugadores(function (jugadores) 
+    {
+        $('#info').replaceWith('<div id="jugadores registrados:">'+"Jugadores registrados: "+ JSON.stringify(jugadores) +'</div>')
     });
 
     var input = $('#value-input')
-    var mensaje = $('#mensaje')
-
-    //Handle delete buttons
-    mensaje.click(function (event) {
-        var elem = $(event.target);
-        if (elem.is('button')) {
-            var chatDiv = elem.parent();
-            var chatId = chatDiv.attr('id').split('-')[1];
-            chatDiv.remove()
-            deleteChat(chatId);
-        }
-    })
-
-    //Handle Chats checkboxs
-    mensaje.change(function (event) {
-
-        //Get page elements for Chat
-        var chatDiv = checkbox.parent();
-        var textSpan = chatDiv.find('span');
-
-        //Read Chat mensaje from elements
-        var chatDescription = textSpan.text();
-        var chatId = chatDiv.attr('id').split('-')[1];
-
-        //Create updated Chat
-        var updatedChat = {
-            id: chatId,
-            value: chatDescription,
-        }
-
-        //Update Chat in server
-        updateChat(updatedChat);
-
-        //Update page when checked
-        var style = chatChecked ? 'line-through' : 'none';
-        textSpan.css('text-decoration', style);
-
-    })
 
     //Handle add button
-    $("#add-but").click(function () {
-
-        var valor = input.val();
+    $("#add-but").click(function () 
+    {
+    	console.log(iplocal);
+        var value = input.val();
         input.val('');
 
-        var chat = {
-            value: valor
+        var jugador = {
+        	ip: iplocal,
+            nombre: value
         }
-
-        createChat(chat, function (chatWithId) {
-            //When Chat with id is returned from server
-            showChat(chatWithId);
-            ref++;
+        
+        createJugador(jugador, function (jugadorWithIp) {
+            //When jugador with ip is returned from server
+            //showJugador(jugadorWithIp);
         });
     })
+    
 })
 
